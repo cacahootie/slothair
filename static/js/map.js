49a231e3bdf8c0ee877f05fec52e1f4cf605dd21
Brad Smith <brad.alan.smith@gmail.com>
2015-06-08 20:13:31 -0700

    basic route viewer.

--- static/js/map.js
+++ static/js/map.js
@@ -1,27 +1,49 @@
-var map;
+var map,
+    display_layer,
+    source_layer;
 
 function draw_map (d) {
-	try {
-		map.remove();
-	} catch (e) {
-		console.log('lol')
-	}
-	map = L.map('map').setView([30, -110], 6);
+	if (display_layer) map.removeLayer(display_layer);
 
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
+	display_layer = L.layerGroup();
 
 	d['results'].forEach(function(dd) {
-		L.marker([dd['lat'],dd['lng']])
+		L.marker([dd.lat,dd.lng])
+			.bindPopup(dd.iata_faa_id)
+			.on('mouseover', function show_tooltip () { this.openPopup(); })
+			.on('mouseout', function hide_tooltip () { this.closePopup(); })
 			.on('click', function map_click () {
-				d3.select('#info_window').text(dd.name);
+				try { map.removeLayer(source_layer); } catch (e) {}
+
+				source_layer = L.layerGroup();
+				
+				var circle = L.circleMarker([dd.lat, dd.lng], {
+					color: 'red',
+					radius: 20,
+					weight: 10,
+					fillColor: 'green',
+					fillOpacity: 0.5
+				}).addTo(source_layer)
+
+				source_layer.addTo(map);
+
+				d3.select('#info_window').html(
+					"Non Stop from <br />" + dd.name + '<br />' + dd.city
+					+ '<br />' + dd.country
+				);
+
 				d3.json('/routes/' + dd.iata_faa_id, draw_map);
 			})
-			.addTo(map);
+			.addTo(display_layer);
 	})
+
+	display_layer.addTo(map);
 }
 
 $(window).load(function () {
+	map = L.map('map').setView([30, -110], 6);
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
 	d3.json('/sources', draw_map);
 })
