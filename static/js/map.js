f8827e67cb5342b279f891af0d62ef848838e581
Brad Smith <brad.alan.smith@gmail.com>
2015-06-09 21:02:51 -0700

    header and marker formatting

--- static/js/map.js
+++ static/js/map.js
@@ -4,18 +4,26 @@ var map,
 
 function draw_map (d) {
 	if (display_layer) map.removeLayer(display_layer);
-
 	display_layer = L.layerGroup();
-	var result_div = d3.select('#result_inner');
+
+	var result_div = d3.select('#result_inner')
 	result_div.html('')
 
+	var detail;
 	d['results'].forEach(function(dd) {
+		detail = dd.city + '<br />' + dd.iata_faa_id;
 		result_div.append('div')
-			.html(dd.name + '<br />' + dd.city + '<br />' + dd.iata_faa_id)
+			.html(detail)
 			.classed('result',true)
 
-		L.marker([dd.lat,dd.lng])
-			.bindPopup(dd.iata_faa_id)
+		L.circleMarker([dd.lat,dd.lng], {
+			color: 'red',
+			fillColor: 'blue',
+			radius: 10,
+			weight: 5,
+			fillOpacity: 0.5
+		})
+			.bindPopup(detail, {offset: L.point(0,-10)})
 			.on('mouseover', function show_tooltip () { this.openPopup(); })
 			.on('mouseout', function hide_tooltip () { this.closePopup(); })
 			.on('click', function map_click () {
@@ -25,7 +33,7 @@ function draw_map (d) {
 				
 				var circle = L.circleMarker([dd.lat, dd.lng], {
 					color: 'red',
-					radius: 20,
+					radius: 14,
 					weight: 10,
 					fillColor: 'green',
 					fillOpacity: 0.5
@@ -46,10 +54,12 @@ function draw_map (d) {
 	display_layer.addTo(map);
 }
 
+var attrtext = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="http://openflights.org">OpenFlights</a>'
+
 $(window).load(function () {
 	map = L.map('map').setView([30, -110], 6);
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	    attribution: attrtext
 	}).addTo(map);
 	d3.json('/sources', draw_map);
 })
