e45d8ddb412c0822890edf18c5ab089ec9425c6b
Brad Smith <brad.alan.smith@gmail.com>
2015-10-14 19:44:59 -0700

    models module

--- slothair/__init__.py
+++ slothair/__init__.py
@@ -10,6 +10,7 @@ from flask import Flask, jsonify, render_template, redirect, request
 from forms import FlightSearchForm
 from interfaces.qpx import get_routes, get_sorted
 from formatters import isodate_time, duration, currency_symbol
+import models
 
 
 basedir = os.path.dirname(os.path.abspath(__file__))
@@ -70,55 +71,14 @@ def search_results():
 
 @app.route("/routes/<source>")
 def routes(source):
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-        cur.execute("""
-    		SELECT distinct(dest_iata), airports.* FROM ROUTES
-    		JOIN AIRPORTS
-    			ON routes.dest_id = airports.airport_id
-    		WHERE 
-    			source_id = (
-    				SELECT airport_id FROM AIRPORTS
-    				WHERE iata_faa_id = %(source_iata)s
-    			)
-    		;""",
-    		{
-    			'source_iata': source
-    		}
-    	)
-    	destinations = [dict(r) for r in cur]
-    	return jsonify({
-    		'results':destinations,
-    		'source_id': source,
-    		'numresults': len(destinations)
-    	})
+    return jsonify(models.routes(source))
 
 @app.route("/airport/<iata>")
 def airport(iata):
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-        cur.execute("""
-            SELECT * FROM AIRPORTS
-            WHERE IATA_FAA_ID = %(iata)s
-            ;""",
-            {
-                'iata': iata
-            }
-        )
-        return jsonify([dict(r) for r in cur][0])
+    return jsonify(models.airport(iata))
 
 @app.route("/sources")
 def sources():
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-        cur.execute("""
-            SELECT NUM_ROUTES, AIRPORTS.* FROM ROUTES_PER_AIRPORT
-            JOIN AIRPORTS
-                ON ROUTES_PER_AIRPORT.source_id = AIRPORTS.airport_id
-            WHERE NUM_ROUTES > 5
-            ;"""
-        )
-        sources = [dict(r) for r in cur]
-        return jsonify({
-            'results':sources,
-            'numresults': len(sources)
-        })
+    return jsonify(models.sources())
 
 
---
+++ slothair/models/__init__.py
@@ -0,0 +1,59 @@
+
+import psycopg2
+import psycopg2.extras
+
+try:
+    conn = psycopg2.connect(dbname='slothair', user='slothair')
+except psycopg2.OperationalError:
+    time.sleep(3)
+    conn = psycopg2.connect(dbname='slothair', user='slothair')
+
+def routes(source):
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+        cur.execute("""
+            SELECT distinct(dest_iata), airports.* FROM ROUTES
+            JOIN AIRPORTS
+                ON routes.dest_id = airports.airport_id
+            WHERE 
+                source_id = (
+                    SELECT airport_id FROM AIRPORTS
+                    WHERE iata_faa_id = %(source_iata)s
+                )
+            ;""",
+            {
+                'source_iata': source
+            }
+        )
+        destinations = [dict(r) for r in cur]
+        return {
+            'results':destinations,
+            'source_id': source,
+            'numresults': len(destinations)
+        }
+
+def airport(iata):
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+        cur.execute("""
+            SELECT * FROM AIRPORTS
+            WHERE IATA_FAA_ID = %(iata)s
+            ;""",
+            {
+                'iata': iata
+            }
+        )
+        return [dict(r) for r in cur][0]
+
+def sources():
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+        cur.execute("""
+            SELECT NUM_ROUTES, AIRPORTS.* FROM ROUTES_PER_AIRPORT
+            JOIN AIRPORTS
+                ON ROUTES_PER_AIRPORT.source_id = AIRPORTS.airport_id
+            WHERE NUM_ROUTES > 5
+            ;"""
+        )
+        sources = [dict(r) for r in cur]
+        return {
+            'results':sources,
+            'numresults': len(sources)
+        }
